5163f7d972acc9cc1009ed0c561aa443c69cfc62
(Holloway) Chew, Kean Ho <kean.ho.chew@zoralab.com>
2021-09-22 18:25:14 +0800

    pkg/monteur: arranged each data structure into its own source codes
    To avoid getting a file bloated and making things understandable,
    let's organize the data struture into their respective source codes.
    Hence, let's do it.
    
    This patch arranges each data structure into its own source codes
    in pkg/monteur/ directory.
    
    Signed-off-by: (Holloway) Chew, Kean Ho <kean.ho.chew@zoralab.com>

--- pkg/monteur/Metadata.go
+++ pkg/monteur/Metadata.go
@@ -15,72 +15,9 @@
 
 package monteur
 
-import (
-	"fmt"
-	"os"
-
-	toml "github.com/pelletier/go-toml/v2"
-)
-
 // Metadata is the data structure holding the repository software information
 //
 // This data structure is part of App{} data structure.
 type Metadata struct {
 	Name string
-}
-
-// App is the main software information consolidated in a data structure.
-//
-// This data structure is unsafe to create using the conventional &struct{}
-// manner. It contains elements that requires independent initialization. Hence,
-// please use NewApp() function to create one.
-type App struct {
-	Metadata *Metadata
-}
-
-// NewApp() is to create an initialized App data structure returning as pointer.
-//
-// This function initializes the *App structure so that it's safe to use upon
-// creations.
-func NewApp() *App {
-	return &App{
-		Metadata: &Metadata{},
-	}
-}
-
-// ParseWorkspace() is the method to parse the repository workspace settings.
-//
-// This single method shall parse and validate the entire repository before
-// executing any CI jobs.
-func (a *App) ParseWorkspace() error {
-	err := a.parseTOML("./../.configs/monteur/appdata/en.toml", true)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (a *App) parseTOML(path string, update bool) error {
-	if path == "" {
-		return fmt.Errorf("missing app toml data file")
-	}
-
-	f, err := os.Open(path)
-	if err != nil {
-		return err
-	}
-
-	decoder := toml.NewDecoder(f)
-
-	err = decoder.Decode(&a)
-	if err != nil {
-		return err
-	}
-
-	if update {
-		fmt.Printf("TESTING: %#v\n", a.Metadata.Name)
-	}
-
-	return nil
 }
