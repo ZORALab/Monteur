9222c90447139d6cf0e4d124c074be2d9cf887e3
(Holloway) Chew, Kean Ho <kean.ho.chew@zoralab.com>
2021-11-29 13:35:24 +0800

    gopkg/monteur/internal: added liblog for Monteur's specific Logging
    Since the genereral logger package can't be used directly on Monteur's
    main source codes, we need a liblog to interface both of them. Hence,
    let's do it.
    
    This patch adds liblog package into gopkg/monteur/internal/
    directory.
    
    Signed-off-by: (Holloway) Chew, Kean Ho <kean.ho.chew@zoralab.com>

---
+++ gopkg/monteur/internal/liblog/Logger.go
@@ -0,0 +1,133 @@
+// Copyright 2021 ZORALab Enterprise (hello@zoralab.com)
+// Copyright 2021 "Holloway" Chew, Kean Ho (hollowaykeanho@gmail.com)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package liblog
+
+import (
+	"fmt"
+	"os"
+
+	"gitlab.com/zoralab/monteur/gopkg/monteur/internal/logger"
+)
+
+const (
+	TYPE_STATUS = logger.TYPE_STATUS
+	TYPE_OUTPUT = logger.TYPE_OUTPUT
+)
+
+type Logger struct {
+	executor      *logger.Logger
+	statusWriters map[string]*os.File
+	outputWriters map[string]*os.File
+
+	DebugMode bool
+}
+
+// Init is to initialize the logger for use.
+func (log *Logger) Init() (err error) {
+	log.executor = logger.New()
+	log.statusWriters = map[string]*os.File{}
+	log.outputWriters = map[string]*os.File{}
+
+	return nil
+}
+
+// Add is to add a given filepath for creating the io.Writer for logger.
+func (log *Logger) Add(logType logger.StatusType, path string) (err error) {
+	f, err := logger.CreateFile(path)
+	if err != nil {
+		return fmt.Errorf("%s: %s", "failed to open log file", err)
+	}
+
+	err = log.executor.Add(f, logType, path)
+	if err != nil {
+		return fmt.Errorf("%s: %s", "failed to register log file", err)
+	}
+
+	switch logType {
+	case TYPE_STATUS:
+		log.statusWriters[path] = f
+	case TYPE_OUTPUT:
+		log.outputWriters[path] = f
+	}
+
+	return nil
+}
+
+// Sync is to sync all the Writers to ensure all data are written to files.
+func (log *Logger) Sync() {
+	for _, f := range log.outputWriters {
+		f.Sync()
+	}
+
+	for _, f := range log.statusWriters {
+		f.Sync()
+	}
+}
+
+// Close is to close all the Writers for the logger safely.
+func (log *Logger) Close() {
+	for _, f := range log.outputWriters {
+		f.Sync()
+		f.Close()
+	}
+
+	for _, f := range log.statusWriters {
+		f.Sync()
+		f.Close()
+	}
+}
+
+// Error is to log an error statement straight to status type logs.
+func (log *Logger) Error(format string, a ...interface{}) {
+	log.executor.Logf(logger.TYPE_STATUS, logger.TAG_ERROR, format, a...)
+}
+
+// Warning is to log a warning statement straight to status type logs.
+func (log *Logger) Warning(format string, a ...interface{}) {
+	log.executor.Logf(logger.TYPE_STATUS, logger.TAG_WARNING, format, a...)
+}
+
+// Info is to log an info statement straight to status type logs.
+func (log *Logger) Info(format string, a ...interface{}) {
+	log.executor.Logf(logger.TYPE_STATUS, logger.TAG_INFO, format, a...)
+}
+
+// Success is to log a success statement straight to status type logs.
+func (log *Logger) Success(format string, a ...interface{}) {
+	log.executor.Logf(logger.TYPE_STATUS, logger.TAG_SUCCESS, format, a...)
+}
+
+// Debug is to log a debug statement straight to status type logs.
+func (log *Logger) Debug(format string, a ...interface{}) {
+	if !log.DebugMode {
+		return
+	}
+
+	log.executor.Logf(logger.TYPE_STATUS, logger.TAG_DEBUG, format, a...)
+}
+
+// Output is to log an output statements straight to status and output logs.
+func (log *Logger) Output(format string, a ...interface{}) {
+	format = "[ OUTPUT ] " + format
+	log.executor.Logf(logger.TYPE_STATUS, logger.TAG_NO, format, a...)
+	log.executor.Logf(logger.TYPE_OUTPUT, logger.TAG_NO, format, a...)
+}
+
+// Logf is to log a raw statement straight to the selected logs' type.
+func (log *Logger) Logf(logType logger.StatusType,
+	format string, a ...interface{}) {
+	log.executor.Logf(logType, logger.TAG_NO, format, a...)
+}
