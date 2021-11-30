8cfbf7bf91943f8004f4fc56b2d7f0614f0e2900
(Holloway) Chew, Kean Ho <kean.ho.chew@zoralab.com>
2021-11-30 09:15:58 +0800

    gopkg/monteur/internal: added conductor package
    Since we are going to use asynchonous orchestration for every API
    executions, it's better we abstract and generate all the orchestration
    implementations into its dedicated package for upstream and Monteur
    use. This way, it's sane enough to maintain all the APIs and upstream
    capable for scaling to other projects. Hence, let's do it.
    
    This patch adds condcutor package into gopkg/monteur/internal
    directory.
    
    Signed-off-by: (Holloway) Chew, Kean Ho <kean.ho.chew@zoralab.com>

---
+++ gopkg/monteur/internal/conductor/Conductor.go
@@ -0,0 +1,278 @@
+// Copyright 2021 ZORALab Enterprise (hello@zoralab.com)
+// Copyright 2021 "Holloway" Chew, Kean Ho (hollowaykeanho@gmail.com)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package conductor
+
+import (
+	"context"
+	"fmt"
+)
+
+const (
+	jobNotDone      uint = 0
+	jobDone         uint = 1
+	jobAllCompleted uint = 2
+)
+
+// Conductor is the coordinators for executing multiple Jobs in parallel.
+//
+// This is similar to conducting an orchestra in a theatre where the main
+// conductor coordinates various musicians to make a good presentation.
+//
+// Conductor is safe to be created using the standard `&struct{}` method.
+type Conductor struct {
+	ctx     context.Context
+	channel chan Message
+
+	stop func()
+
+	// Log is the control object for logging purposes
+	Log Logger
+
+	// Runners are the list of Jobs to be executed in parallel
+	Runners map[string]Job
+
+	hasInitialized bool
+}
+
+func (me *Conductor) init() {
+	if me.hasInitialized {
+		return
+	}
+
+	chLength := len(me.Runners)
+	if chLength == 0 {
+		chLength = 2
+	}
+
+	me.channel = make(chan Message, chLength*2)
+	me.ctx, me.stop = context.WithCancel(context.Background())
+
+	me.hasInitialized = true
+}
+
+// Run is to start the parallel executions
+//
+// If the Conductor.Runners is empty, this function will return an error as
+// there is nothing for execution to begin with.
+func (me *Conductor) Run() (err error) {
+	if len(me.Runners) == 0 {
+		return fmt.Errorf(ERROR_JOBLESS)
+	}
+
+	me.init()
+
+	for _, program := range me.Runners {
+		me.logInfo("Starting Job '%s' in background...", program.Name())
+		go program.Run(me.ctx, me.channel)
+		me.logSuccess("➤ OK\n")
+	}
+
+	return nil
+}
+
+// Coordinate is to manage the coordinatations between the jobs
+//
+// If the Conductor.Runners is empty, this function will return an error as
+// there is nothing for execution to begin with.
+//
+// Otherwise, should any of the job returns an error, Conductor will stop the
+// orchestra entirely and report the error.
+func (me *Conductor) Coordinate() (err error) {
+	var msg Message
+	var ok bool
+
+	if len(me.Runners) == 0 {
+		me.logWarning(ERROR_JOBLESS)
+		return nil
+	}
+
+	me.logInfo("Coordinating running jobs orchestra...")
+	for {
+		select {
+		case <-me.ctx.Done():
+			return nil
+		case msg, ok = <-me.channel:
+			if !ok {
+				me.logWarning(ERROR_CHANNEL_CLOSED)
+				return nil
+			}
+
+			switch me.checkDone(msg) {
+			case jobDone:
+				continue
+			case jobAllCompleted:
+				return nil
+			case jobNotDone:
+				fallthrough
+			default:
+			}
+
+			err = me.checkError(msg)
+			if err != nil {
+				me.stop()
+				return err
+			}
+
+			me.checkStatus(msg)
+		}
+	}
+}
+
+func (me *Conductor) checkStatus(msg Message) {
+	var name string
+	var ok bool
+	var rmsg, rname interface{}
+	var status string
+
+	// obtain message and owner from Message
+	rmsg, ok = msg.Get(CHMSG_STATUS)
+	if !ok {
+		return
+	}
+
+	rname, ok = msg.Get(CHMSG_OWNER)
+	if ok {
+		name, ok = rname.(string)
+		if !ok {
+			name = ""
+		}
+	}
+
+	// handle coordinated display printout
+	status, ok = rmsg.(string)
+	if !ok {
+		return
+	}
+
+	// log the status
+	if name != "" {
+		me.logInfo("Job '%s': %s\n", name, status)
+	} else {
+		me.logInfo("Job '': %s\n", status)
+	}
+}
+
+func (me *Conductor) checkError(msg Message) (err error) {
+	var name string
+	var ok bool
+	var rmsg, rname interface{}
+
+	// obtain message and owner from Message
+	rmsg, ok = msg.Get(CHMSG_ERROR)
+	if !ok {
+		return nil
+	}
+
+	rname, ok = msg.Get(CHMSG_OWNER)
+	if ok {
+		name, ok = rname.(string)
+		if !ok {
+			name = ""
+		}
+	}
+
+	// obtain error object
+	err, ok = rmsg.(error)
+	if !ok {
+		return nil
+	}
+
+	// log the output before returning error
+	if name != "" {
+		me.logError("Got an Error from Job '%s': %s\n", name, err)
+	} else {
+		me.logError("Got an Error: %s\n", err)
+	}
+
+	// return error for conductor to stop the orchestra
+	return err
+}
+
+func (me *Conductor) checkDone(msg Message) (state uint) {
+	var name string
+	var ok, isDone bool
+	var rname, rmsg interface{}
+
+	state = jobNotDone
+
+	rmsg, ok = msg.Get(CHMSG_DONE)
+	if !ok {
+		return state
+	}
+
+	rname, ok = msg.Get(CHMSG_OWNER)
+	if !ok {
+		return state
+	}
+
+	isDone, ok = rmsg.(bool)
+	if !ok {
+		return state
+	}
+
+	if !isDone {
+		return state
+	}
+
+	name, ok = rname.(string)
+	if !ok {
+		return state
+	}
+
+	// a job is done
+	state = jobDone
+	delete(me.Runners, name)
+	me.logInfo("Job '%s' = COMPLETED", name)
+
+	if len(me.Runners) == 0 {
+		state = jobAllCompleted
+		me.logSuccess("All Jobs are COMPLETED.")
+	}
+
+	return state
+}
+
+func (me *Conductor) logError(format string, a ...interface{}) {
+	if !loggerAvailable(me.Log) {
+		return
+	}
+
+	me.Log.Error(format, a...)
+}
+
+func (me *Conductor) logWarning(format string, a ...interface{}) {
+	if !loggerAvailable(me.Log) {
+		return
+	}
+
+	me.Log.Warning(format, a...)
+}
+
+func (me *Conductor) logSuccess(format string, a ...interface{}) {
+	if !loggerAvailable(me.Log) {
+		return
+	}
+
+	me.Log.Success(format, a...)
+}
+
+func (me *Conductor) logInfo(format string, a ...interface{}) {
+	if !loggerAvailable(me.Log) {
+		return
+	}
+
+	me.Log.Info(format, a...)
+}
---
+++ gopkg/monteur/internal/conductor/Message.go
@@ -0,0 +1,129 @@
+// Copyright 2021 ZORALab Enterprise (hello@zoralab.com)
+// Copyright 2021 "Holloway" Chew, Kean Ho (hollowaykeanho@gmail.com)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package conductor
+
+import (
+	"fmt"
+	"sync"
+)
+
+const (
+	CHMSG_DONE   = "done"
+	CHMSG_ERROR  = "error"
+	CHMSG_OWNER  = "owner"
+	CHMSG_STATUS = "status"
+)
+
+// Message is the interface for message payload used in Go channel tramissions.
+type Message interface {
+	Add(k string, v interface{})
+	Get(k string) (v interface{}, ok bool)
+	Update(k string, v interface{})
+	Delete(k string)
+}
+
+type message struct {
+	payload map[string]interface{}
+	lock    *sync.Mutex
+}
+
+// Add is to add a key-value into Message interfacing object.
+func (ch *message) Add(k string, v interface{}) {
+	ch.lock.Lock()
+	defer ch.lock.Unlock()
+
+	ch.payload[k] = v
+}
+
+// Get is to get a value from a given key.
+//
+// It returns the value and the status of query. The status shall be checked
+// before using the value for failure handling.
+func (ch *message) Get(k string) (v interface{}, ok bool) {
+	ch.lock.Lock()
+	defer ch.lock.Unlock()
+
+	v, ok = ch.payload[k]
+	return v, ok
+}
+
+// Update is to update or add a key-value into Message interfacing object.
+//
+// This is a mirror to Add() function.
+func (ch *message) Update(k string, v interface{}) {
+	ch.lock.Lock()
+	defer ch.lock.Unlock()
+
+	ch.Add(k, v)
+}
+
+// Delete is to delete a key-value from the Message interfacing object.
+//
+// The function deletes the key-value regardless of its existence.
+func (ch *message) Delete(k string) {
+	ch.lock.Lock()
+	defer ch.lock.Unlock()
+
+	delete(ch.payload, k)
+}
+
+// NewMessage is to create the initialized Message interface object.
+//
+// You need to construct your own message data that can or cannot be understood
+// by Conductor. This is useful when you use Message outside of Conductor
+// (e.g. you develop your own synchonization mechanism).
+func NewMessage() Message {
+	return &message{
+		payload: map[string]interface{}{},
+		lock:    &sync.Mutex{},
+	}
+}
+
+// CreateStatus creates a standard Message object for Conductor.
+//
+// It takes 2 inputs: the Job owner name and the fmt.Printf like message.
+func CreateStatus(owner string, format string, a ...interface{}) Message {
+	m := NewMessage()
+
+	m.Add(CHMSG_OWNER, owner)
+	m.Add(CHMSG_STATUS, fmt.Sprintf(format, a...))
+
+	return m
+}
+
+// CreateError creates an error Message object for Conductor.
+//
+// It takes 2 inputs: the Job owner name and the fmt.Errorf like error message.
+func CreateError(owner string, format string, a ...interface{}) Message {
+	m := NewMessage()
+
+	m.Add(CHMSG_OWNER, owner)
+	m.Add(CHMSG_ERROR, fmt.Errorf(format, a...))
+
+	return m
+}
+
+// CreateDone creates a done Message object for Conductor.
+//
+// It takes 1 input: the Job owner name.
+func CreateDone(owner string) Message {
+	m := NewMessage()
+
+	m.Add(CHMSG_OWNER, owner)
+	m.Add(CHMSG_DONE, true)
+
+	return m
+}
