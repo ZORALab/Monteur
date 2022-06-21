d8b28eb6ec9c249085c0ad4c571b72edfcb49dc3
(Holloway) Chew, Kean Ho <kean.ho.chew@zoralab.com>
2022-06-21 11:33:13 +0800

    .configs/monteur/prepare/jobs: updated version-file recipes
    Since we are rolling out new recipes for version file scripting,
    we should apply it as well. Hence, let's do this.
    
    This patch updates .configs/monteur/prepare/jobs to use the latest
    version-file scripting recipe.
    
    Signed-off-by: (Holloway) Chew, Kean Ho <kean.ho.chew@zoralab.com>

--- gopkg/monteur/internal/libmonteur/Version.go
+++ gopkg/monteur/internal/libmonteur/Version.go
@@ -11,9 +11,10 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+//
+//
 // **WARNING**: auto-generated file. Please alter at:
-// .configs/monteur/app/config/en/metadata.toml
+//                .configs/monteur/app/config/en/metadata.toml
 
 package libmonteur
 
